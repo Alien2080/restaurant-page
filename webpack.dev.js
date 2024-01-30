9f59e4ce5c9582f300b11c966f29c5cc4f1cd309
Alien2080 <tom_aylen@hotmail.com>
2024-01-30 21:28:16 +1030

    broken old config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,17 +1,10 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    static: './dist/index-dev.html',
+    static: './dist',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-      filename: 'index-dev.html',
-    }),
-  ],
 });

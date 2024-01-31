ed1d240ba7941d43a04e77cfa5392e0afd0408cd
Alien2080 <tom_aylen@hotmail.com>
2024-01-31 22:18:52 +1030

    pages deploy scipt moved to npm packge.json

--- gitpages-deploy.sh
+++
@@ -1,5 +0,0 @@
-# Commits and pushes to branch, the dist folder.
-# Used to update GitHub Pages that expects index.html to be in root folder of a branch.
-# Run the following in WSL terminal:  sh gitpages-deploy.sh 
-
-git add dist -f && git commit -m "update github page with latest build" && git subtree push -f --prefix dist origin gh-pages

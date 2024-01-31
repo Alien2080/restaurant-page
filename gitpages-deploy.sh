14b3999323df7acfe009183a49cf7d2bb82cd08b
Alien2080 <tom_aylen@hotmail.com>
2024-01-31 21:25:16 +1030

    add dist to git ignote again and force add in script

--- gitpages-deploy.sh
+++ gitpages-deploy.sh
@@ -2,4 +2,6 @@
 # Used to update GitHub Pages that expects index.html to be in root folder of a branch.
 # Run the following in WSL terminal:  sh gitpages-deploy.sh 
 
-git add dist && git commit -m "update github page with latest build" && git subtree push --prefix dist origin gh-pages
+git add dist -f
+git commit -m "update github page with latest build" 
+git subtree push --prefix dist origin gh-pages
